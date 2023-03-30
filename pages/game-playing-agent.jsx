c0e580a634bedc08a85119fa2f2840a156ec645b
rebecca0201 <80610101+rebecca0201@users.noreply.github.com>
2023-03-30 21:23:00 +0800

    update project page layout

--- pages/game-playing-agent.jsx
+++ pages/game-playing-agent.jsx
@@ -1,73 +1,44 @@
-import Image from 'next/image';
 import React from 'react';
-import gameImg from '../public/assets/projects/game.png';
-import { RiRadioButtonFill } from 'react-icons/ri';
-import Link from 'next/link';
 
-const netflix = () => {
+const game = () => {
   return (
     <div className='w-full'>
-      <div className='w-screen h-[50vh] relative'>
-        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
-        <Image
-          className='absolute z-1'
-          layout='fill'
-          objectFit='cover'
-          src={gameImg}
-          alt='/'
-        />
-        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>Game-Playing Agent</h2>
-          <h3>Apr 2022 - May 2022</h3>
+
+      <div className='w-full h-[25vh] relative bg-[#5651e5]'>
+        <div className='max-w-[1240px] mx-auto p-2 py-20'>
+          <h2 className='text-white'>Game-Playing Agent</h2>
+          <h3 className='text-white'>Apr 2022 - May 2022</h3>
         </div>
       </div>
 
-      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
-        <div className='col-span-4'>
-          <p>Project</p>
-          <h2>Overview</h2>
-          <p>
-            Cachex Game-Playing Agent is a Python program that excels in playing the Cachex 
-            game, a variant of the Hex game that allows players to capture opponent pieces. 
-          </p>
-          <p>
-            Cachex Game-Playing Agent's significant strengths lie in its strategic approach 
-            to move exploration, which enables it to consider as many possible moves as 
-            possible and make optimal decisions within the time limit. This is achieved 
-            through its utilization of the Minimax algorithm with Alpha-Beta pruning that 
-            enables the agent to search the game tree while discarding unnecessary branches.
-          </p>
-          <p>
-            Additionally, Cachex Game-Playing Agent's efficiency is further improved through 
-            its ability to quickly identify instant winning and losing positions and 
-            strategically explore move orders. It prioritizes those moves that are most likely 
-            to lead to victory, thereby making alpha-beta pruning more effective. As a result, 
-            Cachex Game-Playing Agent achieves an average win rate of 85% against a variety of 
-            strategies.
+      <div className='max-w-[1240px] mx-auto p-2 gap-8 py-8'>
+        <div className='w-full md:w-2/3 mx-4 float-left'>
+          <video className='w-full p-2' src='/assets/projects/game.mp4' controls></video>
+          <p className='text-[#5651e5] p-2'>
+            This video shows the gameplay between the Cachex Game-Playing Agent playing as 
+            blue, and the Monte Carlo Tree Search opponent playing as red.
           </p>
         </div>
-        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
-          <div className='p-2'>
-            <p className='text-center font-bold pb-2'>Technologies</p>
-            <div className='grid grid-cols-3 md:grid-cols-1'>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Python
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Minimax Algorithm
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Alpha-Beta Pruning
-              </p>
-            </div>
-          </div>
-        </div>
-        <Link href='/#projects'>
-          <p className='underline cursor-pointer'>Back</p>
-        </Link>
+        <p className='p-2'>
+          Cachex Game-Playing Agent is a Python program that excels in playing the Cachex 
+          game, a variant of the Hex game that allows players to capture opponent pieces.
+          <br /> <br />
+          Cachex Game-Playing Agent's significant strengths lie in its strategic approach 
+          to move exploration, which enables it to consider as many possible moves as 
+          possible and make optimal decisions within the time limit. This is achieved 
+          through its utilization of the Minimax algorithm with Alpha-Beta pruning that 
+          enables the agent to search the game tree while discarding unnecessary branches.
+          <br /> <br />
+          Additionally, Cachex Game-Playing Agent's efficiency is further improved through 
+          its ability to quickly identify instant winning and losing positions and 
+          strategically explore move orders. It prioritizes those moves that are most likely 
+          to lead to victory, thereby making alpha-beta pruning more effective. As a result, 
+          Cachex Game-Playing Agent achieves an average win rate of 85% against a variety of 
+          strategies.
+        </p>
       </div>
     </div>
   );
 };
 
-export default netflix;
+export default game;
--- pages/recipe-book.jsx
+++ pages/recipe-book.jsx
@@ -1,87 +1,57 @@
-import Image from 'next/image';
 import React from 'react';
-import recipeImg from '../public/assets/projects/recipe.png';
-import { RiRadioButtonFill } from 'react-icons/ri';
-import Link from 'next/link';
 
 const recipe = () => {
   return (
     <div className='w-full'>
-      <div className='w-screen h-[50vh] relative'>
-        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
-        <Image
-          className='absolute z-1'
-          layout='fill'
-          objectFit='cover'
-          src={recipeImg}
-          alt='/'
-        />
-        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>Recipe Book</h2>
-          <h3>Aug 2022 - Oct 2022</h3>
+      <div className='w-full h-[25vh] relative bg-[#5651e5]'>
+        <div className='max-w-[1240px] mx-auto p-2 py-20'>
+          <h2 className='text-white'>Recipe Book</h2>
+          <h3 className='text-white'>Aug 2022 - Oct 2022</h3>
         </div>
       </div>
 
-      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
-        <div className='col-span-4'>
-          <p>Project</p>
-          <h2>Overview</h2>
-          <p>
-            Recipe Book is a Next.js web application that simplifies recipe management for home cooks. 
-            With robust authentication and authorization functionality using Firebase Authentication, 
-            Recipe Book ensures user privacy and security. Users can sign up for an account using their 
-            Google account or email address and access a personalized dashboard to manage their recipes 
-            and profile information.
+      <div className='max-w-[1240px] mx-auto p-2 gap-8 py-8'>
+        <div className='w-full md:w-2/3 mx-4 md:float-left'>
+          <video className='w-full p-2' src='/assets/projects/recipe.mp4' controls></video>
+          <p className='text-[#5651e5] p-2'>
+            This video demonstrates how to sign up for an account using email address, filter recipes 
+            by tags, and add new recipes to collection using Recipe Book.
           </p>
-          <p>
-            Recipe Book's key feature is its recipe management functionality. Users can easily add new 
-            recipes to their collection, complete with photos, ingredients, and step-by-step instructions. 
-            Recipes are stored in Cloud Firestore, and recipe images are stored in Cloud Storage. Users 
-            can edit or delete recipes at any time and organize them with custom tags, making it easy to 
-            find the recipe they need.
-          </p>
-          <p>
-            Our team followed Agile development methodology and held weekly internal meetings to ensure 
-            effective communication and collaboration among team members. We also had weekly stand-up 
-            meetings with our supervisor to report progress and discuss any issues or roadblocks. 
-            Additionally, we had monthly meetings with the client to provide updates on the project's 
-            status and receive feedback. Through this process, we were able to efficiently develop Recipe 
-            Book, meeting expectations and timelines.
-          </p>
-          <a
-            href='https://github.com/haozoo/recipe-book'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
-          </a>
-          <a
-            href='https://re-cipe.vercel.app/'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <button className='px-8 py-2 mt-4'>Demo</button>
-          </a>
         </div>
-        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
-          <div className='p-2'>
-            <p className='text-center font-bold pb-2'>Technologies</p>
-            <div className='grid grid-cols-3 md:grid-cols-1'>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Next.Js
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Tailwind
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Firebase
-              </p>
-            </div>
-          </div>
-        </div>
-        <Link href='/#projects'>
-          <p className='underline cursor-pointer'>Back</p>
-        </Link>
+        <p className='p-2'>
+          Recipe Book is a Next.js web application that simplifies recipe management for home cooks. 
+          With robust authentication and authorization functionality using Firebase Authentication, 
+          Recipe Book ensures user privacy and security. Users can sign up for an account using their 
+          Google account or email address and access a personalized dashboard to manage their recipes 
+          and profile information.
+          <br /> <br />
+          Recipe Book's key feature is its recipe management functionality. Users can easily add new 
+          recipes to their collection, complete with photos, ingredients, and step-by-step instructions. 
+          Recipes are stored in Cloud Firestore, and recipe images are stored in Cloud Storage. Users 
+          can edit or delete recipes at any time and organize them with custom tags, making it easy to 
+          find the recipe they need.
+          <br /> <br />
+          Our team followed Agile development methodology and held weekly internal meetings to ensure 
+          effective communication and collaboration among team members. We also had weekly stand-up 
+          meetings with our supervisor to report progress and discuss any issues or roadblocks. 
+          Additionally, we had monthly meetings with the client to provide updates on the project's 
+          status and receive feedback. Through this process, we were able to efficiently develop Recipe 
+          Book, meeting expectations and timelines.
+        </p>
+        <a
+          href='https://github.com/haozoo/recipe-book'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+        </a>
+        <a
+          href='https://re-cipe.vercel.app/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <button className='px-8 py-2 mt-4'>Demo</button>
+        </a>
       </div>
     </div>
   );
